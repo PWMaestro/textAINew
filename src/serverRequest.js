7bdc080092e899058c53f717fbeb1bd4c75c68d9
Artiom Sitko <48484710+PWMaestro@users.noreply.github.com>
2022-06-06 19:28:43 +0300

    Update serverRequest.js

--- src/serverRequest.js
+++ src/serverRequest.js
@@ -5,7 +5,7 @@ const infoTextArea = document.getElementById("info");
 const sendButton = document.getElementById("send");
 const openButton = document.getElementById("open");
 
-const serverIp = "18.216.205.170";
+const serverIp = "18.218.69.65";
 const regExp = /[^(\d+(.\d+)?)]/g;
 
 const EMPTY_STRING = "";
